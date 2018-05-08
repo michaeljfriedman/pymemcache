0c237e03d91928bf13b93ead5f890cd9bcfbbb38
Michael J Friedman <mfred240@gmail.com>
2018-05-07 22:10:22 -0400

    Implement (new) preliminary load-based server selection
    Select highest-weight server whose load has not surpassed a
    fixed threshold.

--- pymemcache/client/rendezvous_load.py
+++ pymemcache/client/rendezvous_load.py
@@ -11,6 +11,10 @@ class RendezvousLoadHash(object):
   '''
   Rendezvous Hashing based on load information.
   '''
+
+  # Servers with instantaneous load >= this value are considered "loaded"
+  load_threshold = 100
+
   def __init__(self, nodes=None):
     self.nodes = set()
     self.load_manager = LoadManager()
@@ -28,16 +32,22 @@ class RendezvousLoadHash(object):
 
   def get_node(self, key):
     high_score = -1
-    winner = None
+    winner_by_score = None
+    winner_overall = None
 
     server_loads = self.load_manager.load()
 
+    # Pick highest-weight server that isn't loaded. Otherwise default to
+    # server with highest score
     for node in self.nodes:
-        score = murmur3_32("%s-%s-%f" % (node, key, server_loads[node]))
+        score = murmur3_32("%s-%s" % (node, key))
 
         if score > high_score:
-            (high_score, winner) = (score, node)
-        elif score == high_score:
-            (high_score, winner) = (score, max(str(node), str(winner)))
+          (high_score, winner_by_score) = (score, node)
+          if server_loads[node] < RendezvousLoadHash.load_threshold:
+              winner_overall = node
 
-    return winner
+    if not winner_overall:
+      winner_overall = winner_by_score
+
+    return winner_overall
