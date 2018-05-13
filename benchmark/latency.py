2f52fff304bbb95487e987ecd5a20983ceb5597a
Rushy Panchal <panchal.rushy@gmail.com>
2018-05-13 16:51:25 -0400

    Collect benchmark results in main parent process

--- benchmark/latency.py
+++ benchmark/latency.py
@@ -8,6 +8,8 @@ import sys
 import numpy.random
 import hashlib
 import multiprocessing
+import tempfile
+import shutil
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 if not BASE_DIR in sys.path:
@@ -32,41 +34,54 @@ def main():
     host, port = h.split(':')
     parsed_hosts.append((host, int(port)))
 
-  global_client = get_client(client_t, parsed_hosts)
-  global_client.flush_all()
+  initialize_benchmark(client_t, parsed_hosts, count, alpha)
+  
+  # Spawn `concurrency` processes, one per concurrent client.
+  start_time = time.time()
+  processes = {} # maps processes to paths
+
+  for _ in range(concurrency):
+    # Temporary files are used as an easy way to get output from the clients.
+    _, path = tempfile.mkstemp(text=True)
+
+    p = multiprocessing.Process(
+      target=run_benchmark,
+      args=(client_t, parsed_hosts, count, alpha, path))
+
+    processes[p] = path
+    p.start()
+
+  sys.stderr.write('Started %d processes.\n' % concurrency)
+
+  for p in processes.keys():
+    p.join()
+
+  sys.stderr.write('All processes finished in %f seconds.\n' % (time.time() - start_time))
+
+  for path in processes.values():
+    with open(path, 'r') as stream:
+      shutil.copyfileobj(stream, sys.stdout)
+
+  sys.exit(0)
+
+def initialize_benchmark(client_t, parsed_hosts, count, alpha):
+  '''
+  Initialize the benchmark.
+  '''
+  client = get_client(client_t, parsed_hosts)
+  client.flush_all()
 
   for _ in range(count):
     key = str(numpy.random.zipf(alpha))
     value = hashlib.sha256(bytes(key)).digest()
-    global_client.set(key, value)
+    client.set(key, value)
 
-  del global_client
-
-  # Spawn multiple processes for each benchmark.
-  processes = {}
-  for _ in range(concurrency):
-    parent_conn, child_conn = multiprocessing.Pipe(duplex=False)
-    p = multiprocessing.Process(
-      target=run_benchmark,
-      args=(client_t, parsed_hosts, count, alpha, child_conn))
-
-    processes[p] = parent_conn
-    p.start()
-
-  for p, conn in processes.items():
-    p.join()
-
-    for i in range(count):
-      print(conn.recv())
-
-  sys.exit(0)
-
-def run_benchmark(client_t, hosts, count, alpha, conn):
+def run_benchmark(client_t, hosts, count, alpha, path):
   '''
   Run the benchmark.
   '''
   client = get_client(client_t, hosts)
-  times = []
+  stream = open(path, 'w')
 
   for _ in range(count):
     key = str(numpy.random.zipf(alpha))
@@ -75,10 +90,10 @@ def run_benchmark(client_t, hosts, count, alpha, conn):
     client.get(key)
     elapsed = time.time() - start_time
 
-    times.append(elapsed)
+    stream.write(str(elapsed))
+    stream.write('\n')
 
-  for t in times:
-    conn.send(t)
+  stream.close()
 
 def get_client(client_t, hosts):
   '''
