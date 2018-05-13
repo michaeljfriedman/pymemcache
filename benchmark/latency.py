82bb459fcbc8247dc798d70722823b83b665a410
Rushy Panchal <panchal.rushy@gmail.com>
2018-05-13 14:53:35 -0400

    Move all concurrency benchmarking to be inside of latency.py
    The main advantage of this is that each run of latency.py will flush the
    memcached servers (as opposed to each instance of the client, which is not
    an accurate benchmark).

--- benchmark/latency.py
+++ benchmark/latency.py
@@ -1,11 +1,13 @@
 # latency.py
-# Benchmark the memcached server latency across a Zipfian distribution.
+# Benchmark the memcached server latency across a Zipfian distribution with
+# concurrent clients.
 
 import os.path
 import time
 import sys
 import numpy.random
 import hashlib
+import multiprocessing
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 if not BASE_DIR in sys.path:
@@ -16,44 +18,79 @@ from pymemcache.client.rendezvous_load import RendezvousLoadHash
 
 def main():
   if len(sys.argv) < 3:
-    sys.stderr.write('usage: python latency.py COUNT ALPHA CLIENT HOST...\n')
+    sys.stderr.write('usage: python latency.py CONCURRENCY COUNT ALPHA CLIENT_TYPE HOST...\n')
     sys.exit(1)
 
-  count = int(sys.argv[1])
-  alpha = int(sys.argv[2])
-  client_t = sys.argv[3]
-  hosts = sys.argv[4:]
+  concurrency = int(sys.argv[1])
+  count = int(sys.argv[2])
+  alpha = int(sys.argv[3])
+  client_t = sys.argv[4]
+  hosts = sys.argv[5:]
 
   parsed_hosts = []
   for h in hosts:
     host, port = h.split(':')
     parsed_hosts.append((host, int(port)))
 
-  if client_t == 'hash':
-    client = HashClient(servers=parsed_hosts)
-  elif client_t == 'load':
-    client = HashClient(servers=parsed_hosts, hasher=RendezvousLoadHash)
-  else:
-    sys.stderr.write('client_t must be one of: hash, load\n')
-    sys.exit(1)
+  global_client = get_client(client_t, parsed_hosts)
+  global_client.flush_all()
 
-  client.flush_all()
-  key_set = set()
-
-  for i in range(count):
+  for _ in range(count):
     key = str(numpy.random.zipf(alpha))
-    if not key in key_set:
-      key_set.add(key)
-      value = hashlib.sha256(bytes(key)).digest()
-      client.set(key, value)
+    value = hashlib.sha256(bytes(key)).digest()
+    global_client.set(key, value)
+
+  del global_client
+
+  # Spawn multiple processes for each benchmark.
+  processes = {}
+  for _ in range(concurrency):
+    parent_conn, child_conn = multiprocessing.Pipe(duplex=False)
+    p = multiprocessing.Process(
+      target=run_benchmark,
+      args=(client_t, parsed_hosts, count, alpha, child_conn))
+
+    processes[p] = parent_conn
+    p.start()
+
+  for p, conn in processes.items():
+    p.join()
+
+    for i in range(count):
+      print(conn.recv())
+
+  sys.exit(0)
+
+def run_benchmark(client_t, hosts, count, alpha, conn):
+  '''
+  Run the benchmark.
+  '''
+  client = get_client(client_t, hosts)
+  times = []
+
+  for _ in range(count):
+    key = str(numpy.random.zipf(alpha))
 
     start_time = time.time()
     client.get(key)
     elapsed = time.time() - start_time
-    
-    print(elapsed)
 
-  sys.exit(0)
+    times.append(elapsed)
+
+  for t in times:
+    conn.send(t)
+
+def get_client(client_t, hosts):
+  '''
+  Get a client with the given type.
+  '''
+  if client_t == 'hash':
+    return HashClient(servers=hosts)
+  elif client_t == 'load':
+    return HashClient(servers=hosts, hasher=RendezvousLoadHash)
+  else:
+    sys.stderr.write('client_t must be one of: hash, load\n')
+    sys.exit(1)
 
 if __name__ == '__main__':
   main()
